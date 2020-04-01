5ed5ddc24e97ea8871800d153f56d3e1975466d0
Daniel Fillol <daniel_fillol@hotmail.com>
2020-04-01 17:25:33 -0300

    Ajuste no espaçamento dos itens de maneira dinâmica.

--- AplicativoDeViagens/Pacotes/Controller/PacotesViagensViewController.swift
+++ AplicativoDeViagens/Pacotes/Controller/PacotesViagensViewController.swift
@@ -8,16 +8,16 @@
 
 import UIKit
 
-class PacotesViagensViewController: UIViewController, UICollectionViewDataSource{
+class PacotesViagensViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
  
-
-    @IBOutlet weak var colecaoPacotesViagem: UICollectionView!
+    @IBOutlet weak var colecaoPacote: UICollectionView!
     
     let listaViagens: Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        colecaoPacotesViagem.dataSource = self
+        colecaoPacote.dataSource = self
+        colecaoPacote.delegate = self
 
         // Do any additional setup after loading the view.
     }
@@ -28,22 +28,22 @@ class PacotesViagensViewController: UIViewController, UICollectionViewDataSource
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let celulaPacote = collectionView.dequeueReusableCell(withReuseIdentifier: "celulaPacote", for: indexPath) as! PacoteViagemCollectionViewCell
-        
         let viagemAtual = listaViagens[indexPath.item]
-        
         celulaPacote.imagemViagem.image = UIImage(named: viagemAtual.caminhoDaImagem)
         celulaPacote.labelTitulo.text = viagemAtual.titulo
         celulaPacote.labelQuantidadeDias.text = viagemAtual.quantidadeDeDias
-        celulaPacote.labelPreco.text = viagemAtual.preco
-        
+        celulaPacote.labelPreco.text = "R$ \(viagemAtual.preco)"
         celulaPacote.layer.borderWidth = 0.5
         celulaPacote.layer.borderColor = UIColor(red: 85.0/255.0, green: 85.0/255.0, blue: 85.0/255.0, alpha: 1).cgColor
         celulaPacote.layer.cornerRadius = 8
-        
-        
-
         return celulaPacote
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath)-> CGSize {
+        let larguraCelula = collectionView.bounds.width / 2
+        return CGSize(width: larguraCelula - 10, height: 160)
+    }
+
     
 
 
